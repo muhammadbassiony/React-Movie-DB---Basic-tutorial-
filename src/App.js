ae94060adc43a13a555c0dd50df64efd434e2a36
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-10-27 19:27:18 +0200

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+//components
+import Header from './components/Header';
+
+
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Header />
       Start here.
+      
     </div>
   );
 }
