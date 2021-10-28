b14d79312795a38ce16e7bf8b901dfb57d918789
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-10-28 15:43:55 +0200

    homepage landing img

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import API from '../API';
 
+import  HeroImage from './HeroImage';
+
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import { BACKDROP_SIZE, POSTER_SIZE, IMAGE_BASE_URL } from '../config';
@@ -15,7 +17,19 @@ const Home = () => {
 
     console.log(state);
 
-    return <div>Home Page</div>
+    return(
+        <>
+            {
+                state.results[0] ? 
+                <HeroImage  
+                    image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title = {state.results[0].original_title}
+                    text = {state.results[0].overview}
+                /> : null
+            }
+        
+        </>
+    )
 };
 
 export default Home;
