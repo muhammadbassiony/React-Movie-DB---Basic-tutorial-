4e5d87cf1ff8d8dd085f937a1e60154c873ac582
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-10-29 00:16:31 +0200

    movie info page done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
-// import Actor from './Actor';
+import Actor from './Actor';
 
 
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -31,6 +31,16 @@ const Movie = () => {
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
             <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+            <Grid header="ACTORS">
+                {movie.actors.map( actor => (
+                    <Actor 
+                        key={actor.credit_id} 
+                        name={actor.name} 
+                        character={actor.character}
+                        imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
+                    /> 
+                ))}
+            </Grid>
         </>
     );
 };
