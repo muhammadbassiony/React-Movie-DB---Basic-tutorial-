a29dccdc7599adb868ccb3922ae1c1210c40c085
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-10-28 14:40:12 +0200

    homepage API fetch complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,40 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import API from '../API';
 
+import { useHomeFetch } from '../hooks/useHomeFetch';
+
 import { BACKDROP_SIZE, POSTER_SIZE, IMAGE_BASE_URL } from '../config';
 
 import NoImage from '../images/no_image.jpg';
 
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async(page, searchTerm="") => {
-        try{
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            // console.log(movies);
-
-            setState(prev => ({ 
-                ...movies, 
-                results:
-                page>1? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-
-            setLoading(false);
-
-        }catch(error){
-            setError(true);
-        }
-    };
-
-    useEffect(() => {
-        fetchMovies(1);
-    }, []);
+    
+    const { state, loading, error } = useHomeFetch();  
 
     console.log(state);
 
