c239c2626ec239007febfb766286c3b619d248f3
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-10-28 23:20:31 +0200

    movie info page api calls

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+// import BreadCrumb from './BreadCrumb';
+import Grid from './Grid';
+import Spinner from './Spinner';
+// import MovieInfo from './MovieInfo';
+// import MovieInfoBar from './MovieInfoBar';
+// import Actor from './Actor';
 
-const Movie = () => <div>Movie Here.</div>;
+
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log('MMOVIE :: ', movie);
+    
+    return  (
+        <>
+            <div>Movie Here</div>
+        </>
+    );
+};
 
 export default Movie;
